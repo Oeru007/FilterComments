771149f6d3768e43b937d0ddc1df42b23d8c6993
Oeru007 <Oeru007@mail.ru>
2021-10-06 07:09:34 +0300

    Initial commit

---
+++ KeywordAnalyzer.java
@@ -0,0 +1,14 @@
+public abstract class KeywordAnalyzer implements TextAnalyzer {
+    protected abstract String[] getKeywords();
+    protected abstract Label getLabel();
+
+    @Override
+    public Label processText(String text) {
+        for (String str : getKeywords()) {
+            if (text.contains(str)){
+                return getLabel();
+            }
+        }
+        return Label.OK;
+    }
+}
